132bcbd37a8f38d9bbc296cd06a446c9fe0ec8e1
zhangrp <zhangrp@bairuitech.com>
2021-03-01 17:42:15 +0800

    deploy

--- config.js
+++ config.js
@@ -130,7 +130,7 @@ module.exports = {
                     ['/pages/react之小白/3.Redux详解', '3.Redux详解'],
                     ['/pages/react之小白/4.React&redux结合', '4.React&redux结合'],
                     ['/pages/react之小白/5.React-Redux来代替Redux', '5.React-Redux来代替Redux'],
-                    ['/pages/react之小白/6.简述Context的实现', '6.简述Context的实现']
+                    ['/pages/react之小白/6.简述Context的使用和实现', '6.简述Context的使用和实现']
                 ]
             },
             {
